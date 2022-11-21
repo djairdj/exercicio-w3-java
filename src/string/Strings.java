ba7af3e3439acfcb2e783af6061dd6e5e9de50a3
Djair Alves <djair.dj@gmail.com>
2022-11-21 07:51:18 -0300

    Novos Testes

--- src/string/Strings.java
+++ src/string/Strings.java
@@ -1,5 +1,6 @@
 package string;
 
+
 public class Strings {
     public static String ex001(String name) {
         /*
@@ -56,7 +57,7 @@ public class Strings {
     }
 
     public static String ex008() {
-        /*Write a Java program to display the following pattern */
+        /* Write a Java program to display the following pattern */
         String r = """
                    J    a   v     v  a
                    J   a a   v   v  a a
@@ -66,13 +67,123 @@ public class Strings {
     }
 
     public static double ex009(double n) {
-        /* Write a Java program to compute the specified expressions and print the output */
+        /*
+         * Write a Java program to compute the specified expressions and print the
+         * output
+         */
         return n;
     }
 
-    public static double ex010(double n){
-        /*Write a Java program to compute a specified formula */
+    public static double ex010(double n) {
+        /* Write a Java program to compute a specified formula */
         return n;
+    }
+
+    private static double areaCirculo(double raio) {
+        return Math.PI * (raio * raio);
+    }
+
+    private static double areaRetangulo(double lado1, double lado2){
+        return lado1 * lado2;
+    }
+
+    private static double perimetroCirculo(double raio) {
+        return Math.PI * 2 * raio;
+    }
+
+    private static double perimetroRetangulo(double lado1, double lado2){
+        return 2 * (lado1 + lado2);
+    }
+
+    public static String ex011(double radius) {
+        /* Write a Java program to print the area and perimeter of a circle */
+        double p = perimetroCirculo(radius);
+        double a = areaCirculo(radius);
+
+        String per = "Perimeter is = " + p + "\n";
+        String ar = "Area is = " + a;
+        return per + ar;
+    }
+
+    public static double ex012(double n1, double n2, double n3) {
+        /*
+         * Write a Java program that takes three numbers as input
+         * to calculate and print the average of the numbers.
+         */
+        return (n1 + n2 + n3) / 3;
+    }
+
+    public static String ex013(double l1, double l2) {
+        /* Write a Java program to print the area and perimeter of a rectangle */
+        double a = areaRetangulo(l1, l2);
+        double p = perimetroRetangulo(l1, l2);
+        String ar = "Area is " + String.format("%.1f * %.1f = %.2f\n", l1, l2, a);
+        String per = "Perimeter is " + String.format("%d * (%.1f + %.1f) = %.2f\n", 2, l1, l2, p);
+        return ar + per;
+    }
+
+    public static String ex014(){
+        return """
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================
+                """;
+    }
+
+    public static String ex015(int n1, int n2){
+        String f = "";
+        f += "n1 vale "+ n1;
+        f += "\nn2 vale "+ n2;
+        n1 = n2+n1;
+        n2 = n1 - n2;
+        n1 -= n2;
+        f += "\nn1 vale "+ n1;
+        f += "\nn2 vale "+ n2;
+        return f;
+    }
+
+    public static String ex016(){
+        return """
+             +""\"""+                                                 
+            [| o o |]                                                
+             |  ^  |                                                 
+             | '-' |                                                 
+             +-----+
+                """;
+    }
+    
+    public static String ex017(String bin1, String bin2){
+        /*Write a Java program to add two binary numbers */
+        for (int i = 0; i < bin1.length(); i++) {
+            if (bin1.charAt(i) != '0' && bin1.charAt(i) != '1') {
+                return "primeiro argumento não binario";
+            }
+        }
+        for (int i = 0; i < bin2.length(); i++) {
+            if (bin2.charAt(i) != '0' && bin2.charAt(i) != '1') {
+                return "segundo argumento não binaro";
+            }
+        }
+
+        String r = "Sum of two binary numbers: ";
+        int soma = Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2);
+        String b = Integer.toBinaryString(soma);  
+        return r + b;
+    }
+    public static void main(String[] args) {
+        System.out.println(ex017("10", "11"));
     }
 
 }
--- src/string/TestString.java
+++ src/string/TestString.java
@@ -3,6 +3,8 @@ package string;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Locale;
+
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
@@ -110,11 +112,97 @@ public class TestString {
     }
 
     @Test
-    public void ex010(){
+    public void ex010Test(){
         /*Write a Java program to compute a specified formula */
         double expected = 2.9760461760461765;
         assertEquals(expected, Strings.ex010(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))));
     }
 
+    @Test
+    public void ex011Test(){
+        /*Write a Java program to print the area and perimeter of a circle */
+        double radius = 7.5;
+        String expected = "Perimeter is = 47.12388980384689\nArea is = 176.71458676442586";
+        assertEquals(expected, Strings.ex011(radius));
+    }
 
+    @Test
+    public void ex012Test(){
+        /*Write a Java program that takes
+        three numbers as input to calculate and print the average of the numbers. */
+        double a = 20;
+        double b = 80;
+        double c = 50;
+        double m = (a+b+c)/3;
+        assertEquals(m, Strings.ex012(a, b, c));
+    }
+
+    @Test
+    public void ex013Test(){
+        Locale.setDefault(Locale.US);
+        String expected = """
+                Area is 5.6 * 8.5 = 47.60
+                Perimeter is 2 * (5.6 + 8.5) = 28.20 
+                """;
+        double a = 5.6;
+        double b = 8.5;
+        assertEquals(expected, Strings.ex013(a, b));
+    }
+
+    @Test
+    public void ex014Test(){
+        /*Write a Java program to print an American flag on the screen */
+        String flag = """
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                 * * * * *  ==================================                          
+                * * * * * * ==================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================                          
+                ==============================================
+                """;
+                assertEquals(flag, Strings.ex014());
+    }
+
+    @Test
+    public void ex015Test(){
+        /*Write a Java program to swap two variables */
+        int n1 = 3;
+        int n2 = 7;
+        String f = "";
+        f += "n1 vale "+ n1;
+        f += "\nn2 vale "+ n2;        
+        f += "\nn1 vale "+ n2;
+        f += "\nn2 vale "+ n1;
+        assertEquals(f, Strings.ex015(n1, n2));
+    }
+
+    @Test
+    public void ex016Test(){
+        /*Write a Java program to print a face */
+        String f= """
+             +""\"""+                                                 
+            [| o o |]                                                
+             |  ^  |                                                 
+             | '-' |                                                 
+             +-----+
+                """;
+        assertEquals(f, Strings.ex016());
+    }
+
+    @Test
+    public void ex017Test(){
+        String b1 = "10";
+        String b2 = "11";
+        String expected = "Sum of two binary numbers: 101";
+        assertEquals(expected, Strings.ex017(b1, b2));
+    }
 }
